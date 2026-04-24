65a3e33eb3a4d8c94776207ece03d58e14439c41
varsha106-pixel <varshajawadi@gmail.com>
2026-04-24 15:24:32 -0700

    Revert "fixed bugs"
    This reverts commit 997ee90e79e9f17d170725cb7ab1ed56e4b4a60a.

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,14 +7,14 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
-        
+        const result = 0;
 
-        const result = num1 + num2;
+        result = num1 + num2;
 
         return result;
     }
     else {
-        return add;
+        return !add;
     }
 }
 
@@ -27,12 +27,9 @@ function sumValues(num1, num2, add) {
 function discountPrices(prices, discount) {
     const discounted = []
     const length = prices.length;
-    if (length === 0) {
-        return false;
-    }
     let discountedPrice = 0
     for(let i = 0; i < length; i++) {
-        discountedPrice = prices[i] * (1 - discount);
+        discountedPrice += prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
