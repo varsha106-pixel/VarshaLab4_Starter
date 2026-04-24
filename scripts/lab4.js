ec2536a7d4e87de35d3a7e7a81ed2f36e5869d5f
varsha106-pixel <varshajawadi@gmail.com>
2026-04-24 15:38:24 -0700

    add type checks

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,6 +6,9 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return false;
+    }
     if (add) {
     
 
@@ -25,6 +28,10 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+      if (!Array.isArray(prices) || prices.length === 0) {
+    return false;
+  }
+
     const discounted = []
     const length = prices.length;
     if (length === 0) {
